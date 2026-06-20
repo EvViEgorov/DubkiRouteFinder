ac4d9fe30a30d6c7a035efb02e9de9b577135ce1
Sofiia Karamullina <smkaramullina@edu.hse.ru>
2026-06-20 04:10:33 +0300

    add comments

--- core/query.py
+++ core/query.py
@@ -8,12 +8,14 @@ DATA_PATH = BASE_DIR / "data" / "connections_data.json"
 SCHEDULE_PATH = BASE_DIR / "data" / "schedules.json"
 
 def get_sorted_routes(start, end, dep_time, less_outside):
+    # инициализируем граф маршрутной сети с данными и расписанием
     routes = RouteNetwork(DATA_PATH, SCHEDULE_PATH)
+    # получаем все возможные маршруты с учётом времени отправления
     all_routes = routes.get_all_routes(start, end, dep_time)
 
     if less_outside == 1:
+        # меньше времени на улице
         all_routes.sort(key=lambda r: (r[2], r[1]))
     else:
+        # быстрее добраться
         all_routes.sort(key=lambda r: (r[1], r[2]))
-
-    return all_routes
